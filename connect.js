f290846ad8c546c13036997fea81c5796fda610d
aryan1234 <kanwararyan1@gmail.com>
2017-01-16 10:37:23 +0530

    Update connect.js

--- connect.js
+++ connect.js
@@ -1,6 +1,6 @@
 module.exports = {
-  consumer_key:         'G6Q5MyQefOWhLTRnGVhDxWWc7',
-  consumer_secret:      'mCVDeP28rbWg17KOKodet4tqjbtozvV7dXJ7VgHG8wK91xrsN6',
-  access_token:         '819144756521410562-hVSIZvDw7UYfgJhbA16mxjVlRG2EWFn',
-  access_token_secret:  'LQkys6teoMtyutbtHJVpyYDsypRo00ZF9G40RFWiv3jBp',
+  consumer_key:         'XXXXXXXXXXXXXXXXXXXX',
+  consumer_secret:      'XXXXXXXXXXXXXXXXXXX',
+  access_token:         'XXXXXXXXXXXXXXXXXX',
+  access_token_secret:  'XXXXXXXXXXXXXXXXXXX',
 }
